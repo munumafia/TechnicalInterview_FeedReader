d5362813cecbbcbb40b8607e0047f38e9ab52563
Sean Grimes <sean@codemunkey.net>
2013-11-28 09:25:59 -0600

    Adding bootstrap and initial feed screen

--- src/TechnicalInterview_FeedReader/App_Start/BundleConfig.cs
+++ src/TechnicalInterview_FeedReader/App_Start/BundleConfig.cs
@@ -23,7 +23,16 @@ namespace TechnicalInterview_FeedReader
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/jquery-{version}.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/feedreader").Include(
+                "~/Content/FeedReader.css",
+                "~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
