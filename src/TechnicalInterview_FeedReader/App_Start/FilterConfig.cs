0656d45973d482ce795adf072343681b03640f1f
Sean Grimes <sean@codemunkey.net>
2013-11-29 16:23:03 -0600

    Only allowing authorized users to access all parts of the website except for the register and login pages

--- src/TechnicalInterview_FeedReader/App_Start/FilterConfig.cs
+++ src/TechnicalInterview_FeedReader/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace TechnicalInterview_FeedReader
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
